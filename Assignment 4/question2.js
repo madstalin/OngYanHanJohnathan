e1545703e4bea7af245e709faba810142c736e15
madstalin <heil_yanhan@hotmail.com>
2025-12-21 22:16:33 +0800

    Create question2.js

---
+++ Assignment 4/question2.js
@@ -0,0 +1,13 @@
+const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
+
+let sum = 0;
+
+for (let i = 0; i < amdPrices.length; i++) {
+  sum += amdPrices[i];
+  
+  
+}
+
+const average = sum / amdPrices.length;
+
+console.log("The 7-day SMA of AMD is ", average);
