9b2bba084c1e783347a927c778b4f471a6a35332
madstalin <heil_yanhan@hotmail.com>
2025-12-21 21:56:17 +0800

    Update question1.js

--- Assignment 4/question1.js
+++ Assignment 4/question1.js
@@ -1,6 +1,11 @@
 const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
 
-for(let i = 0; i < samdPrices.length; i++) {
-	// Add
-	amdPrices += amdPricesPrices[i]
+let highestPrice = amdPrices[0];
+
+for (let i = 1; i < amdPrices.length; i++) {
+  if (amdPrices[i] > highestPrice) {
+    highestPrice = amdPrices[i];
+  }
 }
+
+console.log("AMD 52-week high is ", highestPrice);
