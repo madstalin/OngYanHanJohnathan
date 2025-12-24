80f95e41100d31fe83e36d1bd2d99391e186f1b4
madstalin <heil_yanhan@hotmail.com>
2025-12-24 15:42:34 +0800

    Update question5.js

--- Assignment 3/question5.js
+++ Assignment 3/question5.js
@@ -1,2 +1,23 @@
 const bacData = [190.15, 194.21, 191.07, 192.44, 129000];
 
+const [low, high, open, close, volume] = bacData;
+
+if (close > open){
+    if (volume > 100000){
+        console.log("Strong Bullish");
+    }
+    else{
+        console.log("Bullish");
+  }
+}
+else if (close < open){
+        if (volume > 10000){
+            console.log("Strong Bearish");
+        }
+        else{
+            console.log("Bearish");
+        }
+  }
+else if (close === open){
+        console.log("Neutral");
+  }
