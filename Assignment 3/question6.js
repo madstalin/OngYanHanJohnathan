083102fabba7ab6f20e1f7f1d53381221a9ed79b
madstalin <heil_yanhan@hotmail.com>
2025-12-24 15:51:38 +0800

    Update question6.js

--- Assignment 3/question6.js
+++ Assignment 3/question6.js
@@ -6,3 +6,28 @@ const disneyData = {
 	volume: "90000"
 }
 
+let open = 120.54;
+let high = 122.56;
+let low = 120.54;
+let close = 121.09;
+let volume = 90000;
+
+if (close > open){
+    if (volume > 100000){
+        console.log("Strong Bullish");
+    }
+    else{
+        console.log("Bullish");
+    }
+}
+else if (close < open){
+    if (volume > 10000){
+        console.log("Strong Bearish");
+    }
+    else{
+        console.log("Bearish");
+    }
+}
+else if (close === open){
+    console.log("Neutral");
+}
