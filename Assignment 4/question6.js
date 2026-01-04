643ba6596078c6c70425f87d62708c2617cb5e50
madstalin <heil_yanhan@hotmail.com>
2026-01-04 23:05:36 +0800

    Create question6.js

---
+++ Assignment 4/question6.js
@@ -0,0 +1,7 @@
+const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82]
+
+const lowestThree = [...amdPrices] 
+  .sort((a, b) => a - b)
+  .slice(0, 3)
+
+console.log("The three lowest prices are", lowestThree);
